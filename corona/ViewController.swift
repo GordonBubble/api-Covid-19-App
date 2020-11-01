803d95fae59c76f8edf366f1fe1cea06c4f2a4dc
Roman Avdonin <gordonbuble49@gmail.com>
2020-11-01 21:20:29 +0300

    Initial Commit

---
+++ corona/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  corona
+//
+//  Created by Яблочко on 01.11.2020.
+//  Copyright © 2020 Roman Avdonin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
